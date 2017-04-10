ba86b6e1f30c8d88a0b074adc0183ba729b0f2c9
Nils Asmussen <nils@script-solution.de>
2017-04-10 13:42:38 +0200

    Specify gem5 dir via env var.

--- nre/tools/gem5.sh
+++ nre/tools/gem5.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-gem5dir=/home/hrniels/imdata/cycle-accurate-sim/hw/gem5
+
+if [ ! -d "$GEM5_DIR" ]; then
+    echo "Please specify the gem5 directory via GEM5_DIR." >&2
+    exit 1
+fi
 
 # Exec,ExecPC,Faults,I82094AA,LocalApic
 gem5args=" --remote-gdb-port=1234 --debug-flags=Faults"
 # gem5args="$gem5args --debug-start=3071984000"
-gem5args="$gem5args $gem5dir/configs/example/fs.py"
+gem5args="$gem5args $GEM5_DIR/configs/example/fs.py"
 gem5args="$gem5args --command-line=\"novga serial\""
 gem5args="$gem5args --caches --l2cache --cpu-type detailed --cpu-clock 1GHz --mem-size=256MB"
 gem5args="$gem5args -n 2"
@@ -25,7 +29,7 @@ if [ "$GUEST_DBG" != "" ]; then
     cmds=`mktemp`
     echo "target remote localhost:1234" > $cmds
 
-    echo $gem5args | M5_PATH=$imgs xargs $1/tools/ignoreint/ignoreint $gem5dir/build/X86/gem5.opt > log.txt 2>&1 &
+    echo $gem5args | M5_PATH=$imgs xargs $1/tools/ignoreint/ignoreint $GEM5_DIR/build/X86/gem5.opt > log.txt 2>&1 &
     gdb --tui -command=$cmds $1/bin/apps/hypervisor-elf64
 
     killall gem5.opt
@@ -35,11 +39,11 @@ elif [ "$GEM5_DBG" != "" ]; then
     echo "b main" > $cmds
     echo "run $gem5args >log.txt" >> $cmds
 
-    M5_PATH=$imgs gdb --tui -command=$cmds $gem5dir/build/X86/gem5.debug
+    M5_PATH=$imgs gdb --tui -command=$cmds $GEM5_DIR/build/X86/gem5.debug
 
     rm -f $cmds
 else
-    echo $gem5args | M5_PATH=$imgs xargs $gem5dir/build/X86/gem5.opt > log.txt &
+    echo $gem5args | M5_PATH=$imgs xargs $GEM5_DIR/build/X86/gem5.opt > log.txt &
     while [ "`lsof -i :3456`" = "" ]; do
         sleep 1
     done
